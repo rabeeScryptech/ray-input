f015ad6c8cb8fe816fa423e3c7ed98b081c74565
rabeeScryptech <39902113+rabeeScryptech@users.noreply.github.com>
2018-12-06 12:56:12 +0200

    Update ray-input.js

--- src/ray-input.js
+++ src/ray-input.js
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import * as THREE from 'three';
 import OrientationArmModel from './orientation-arm-model'
 import EventEmitter from 'eventemitter3'
 import RayRenderer from './ray-renderer'
