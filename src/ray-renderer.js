9f1d07bd22003859c911a03cdaa59d428453ab7b
rabeeScryptech <39902113+rabeeScryptech@users.noreply.github.com>
2018-12-06 12:57:24 +0200

    Update ray-renderer.js

--- src/ray-renderer.js
+++ src/ray-renderer.js
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import * as THREE from 'three';
 import {base64} from './util'
 import EventEmitter from 'eventemitter3'
 
